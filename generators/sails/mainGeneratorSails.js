119e9e5693031a4b98fc830c7cef345dea0e5ab0
Jackson Morgan <jmorgan45@gatech.edu>
2015-09-26 08:56:04 -0400

    Main generator done

--- generators/sails/mainGeneratorSails.js
+++ generators/sails/mainGeneratorSails.js
@@ -1,5 +1,31 @@
+var fs = require('fs-extra');
 
 module.exports = function(folder, info, callback) {
     console.log("Main Generator");
-    callback();
+    // Copy everything over to the right folder
+    fs.copy('./generators/sails/template', folder, function (err) {
+        if (err) {
+            // TODO: Handle Error
+            console.log(err);
+        }
+
+        // Update Package.json
+        fs.readFile(folder + '/package.json', 'utf8', function (err, data) {
+            if (err) {
+                // TODO: Handle Error
+                console.log(err);
+            }
+            var pack = JSON.parse(data);
+            pack.name = info.repoName;
+            pack.description = info.repoDescription;
+            // TODO: Repo Info
+            fs.writeFile(folder + '/package.json', JSON.stringify(pack, null, 4), function(err, data) {
+                if (err) {
+                    // TODO: Handle Error
+                    console.log(err);
+                }
+                callback();
+            });
+        });
+    });
 }
--- generators/sails/mongoGeneratorSails.js
+++ generators/sails/mongoGeneratorSails.js
@@ -1,5 +1,8 @@
 
 module.exports = function(folder, info, callback) {
     console.log("Mongo Generator");
+
+    // Remember to make connections config file
+    // Remember to update models file
     callback();
 }
