cba19c9dc0d05859ace66977ae2f3b4b3c03ed26
Jackson Morgan <jmorgan45@gatech.edu>
2015-09-27 03:33:53 -0400

    Styled general form

--- components/editor/General.js
+++ components/editor/General.js
@@ -3,6 +3,9 @@ var updateInfoAction = require('../../actions/UpdateInfoAction');
 var connectToStores = require("fluxible-addons-react").connectToStores;
 var RequestStore = require('../../stores/RequestStore');
 
+var allowedFrameworks = ['sails'];
+var allowedDatabases = ['mongo'];
+
 var General = React.createClass({
     contextTypes: {
         executeAction: React.PropTypes.func.isRequired
@@ -13,13 +16,29 @@ var General = React.createClass({
     render: function() {
         return (
             <div>
-                <p>General</p>
-                <label for="repoName">Repo Name</label>
-                <input type="text"  id="repoName" ref="repoName" value={this.props.request.repoName} onChange={this.updateInfo.bind(null, ['repoName'])} />
+                <h1>General Information</h1>
+                <div className="restfull-editor-section"><div className="restfull-editor-section-center">
+                    <label for="repoName">Repo Name</label>
+                    <input type="text"  id="repoName" ref="repoName" value={this.props.request.repoName} onChange={this.updateInfo.bind(null, ['repoName'])} />
 
-                <label for="repoDescription">Repo Name</label>
-                <input type="text"  id="repoDescription" ref="repoDescription" value={this.props.request.repoDescription} onChange={this.updateInfo.bind(null, ['repoDescription'])} />
-                <p>{this.props.request.repoName}</p>
+                    <label for="repoDescription">Repo Description</label>
+                    <input type="text"  id="repoDescription" ref="repoDescription" value={this.props.request.repoDescription} onChange={this.updateInfo.bind(null, ['repoDescription'])} />
+                
+                    <label for="framework">Framework</label>
+                    <select id="framework" ref="framework" value={this.props.request.framework} onChange={this.updateInfo.bind(null, ['framework'])}>
+                        {allowedFrameworks.map(function(framework) {
+                            return (<option value={framework}>{framework}</option>);
+                        }.bind(this))}
+                    </select>
+
+                    <label for="database">Database</label>
+                    <select id="database" ref="database" value={this.props.request.database} onChange={this.updateInfo.bind(null, ['database'])}>
+                        {allowedDatabases.map(function(database) {
+                            return (<option value={database}>{database}</option>);
+                        }.bind(this))}
+                    </select>
+
+                </div></div>
             </div>
         );
     }
